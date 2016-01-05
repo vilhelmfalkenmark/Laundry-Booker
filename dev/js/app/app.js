620115ae57c435143559ec6b9c22e82d1e32252f
Vilhelm Falkenmark <vilhelmfalkenmark@gmail.com>
2016-01-05 16:45:14 +0100

    Fixed the calender function so we can switch between weeks

--- dev/js/app/app.js
+++ dev/js/app/app.js
@@ -1,13 +1,38 @@
 var app = angular.module('booker', []);
 
 app.controller('mainController', ['$scope', function($scope) {
+  var milliSeconds = 24 * 60 * 60 * 1000;
+  $scope.daySwitch = 0;
 
-  $scope.date = new Date();
+  function createCalender()
+  {
+    $scope.date = new Date(new Date().getTime() + $scope.daySwitch*milliSeconds);
     $scope.dates = [];
-    var d;
-    for(var i = 0; i < 7; i++){
-       d = $scope.date;
-       d = (d.setDate((d.getDate() + 1)));
-       $scope.dates.push(d);
-    }
+      var d;
+      for(var i = 0; i < 7; i++)
+      {
+          var day = {};
+          d = $scope.date;
+          d = (d.setDate((d.getDate() + 1)));
+          day.dayName = d;
+          $scope.dates.push(day);
+      }
+  }
+  createCalender()
+    $scope.plusWeek = function() {
+
+      if($scope.daySwitch < 14)
+      {
+        $scope.daySwitch += 7;
+        createCalender()
+      }
+  	};
+
+   	 $scope.minusWeek = function() {
+       if($scope.daySwitch > 0)
+       {
+         $scope.daySwitch -= 7;
+         createCalender()
+       }
+  	};
 }]);
