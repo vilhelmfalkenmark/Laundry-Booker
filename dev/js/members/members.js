cc56ed0fdd38e7e95afa25c24178b8e0b3065845
Vilhelm Falkenmark <vilhelmfalkenmark@gmail.com>
2016-01-06 13:13:31 +0100

    Basic styling of right column calender

---
+++ dev/js/members/members.js
@@ -0,0 +1,20 @@
+[
+  {
+    "name": "Ville Falkenmark",
+    "floor": "3",
+    "apartment": "1408",
+    "password": "1234"
+  },
+  {
+    "name": "Simon Lager",
+    "floor": "4",
+    "apartment": "4322",
+    "password": "1234"
+  },
+  {
+    "name": "Fredrik Löfgren",
+    "floor": "4",
+    "apartment": "8109",
+    "password": "1234"
+  }
+]
