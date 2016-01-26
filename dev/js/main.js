34c4f2ba620cfa08adaece2c2269d59e4a7684b6
SimonLager <simonlager@msn.com>
2016-01-26 16:34:15 +0100

    Added som message functionality

--- dev/js/main.js
+++ dev/js/main.js
@@ -251,6 +251,30 @@
 
 })();
 
+angular.module('booker')
+.directive('notification', ['$timeout', function ($timeout) {
+  return {
+    restrict: 'A',
+    controller: ['$scope', function ($scope) {
+      $scope.notification = {
+        status: 'hide',
+        type: 'success',
+        message: 'Welcome! It\'s yet another angular alert ;)'
+      };
+    }],
+    link: function(scope, elem, attrs) {
+      attrs.$observe('notification', function (value) {
+        if (value === 'show') {
+          $(elem).show();
+          $timeout(function () {
+            $(elem).hide();
+            scope.notification.status = 'hide';
+          }, 3000);
+        }
+      });
+    }
+  };
+}]);
 $(document).ready(function () {
     $('.weekdays-container').on('click', '.weekday-expand', function() {
         $('.booking-container').slideUp(500);
